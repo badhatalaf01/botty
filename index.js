e37eb720e8456c09b09a0953a5aa518f402f4fb8
badhatalaf01 <132492184+badhatalaf01@users.noreply.github.com>
2023-05-04 01:48:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ const createBot = (username) => {
         else if (msg.includes('check bal')) bot.chat('/bal')
         else if (msg.includes('Please, login with the command:')) bot.chat('/login bruh51')
         else if (msg.includes('Please type: /captcha ')) {
-            var msgarr = msg.split('captcha ')
-            var captcha = msgarr[1].split(' ').join('')
+            var msgarr = msg.split('Please type: /captcha ')
+            var captcha = msgarr[-1].split(' ').join('')
             bot.chat('/captcha ' + captcha)
         }
     }
